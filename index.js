0e25113536a87a08e970131ec3f531608dc87ec9
gus-wip <gustavoocosta@icloud.com>
2023-12-11 23:10:43 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("olá")
+//
+
+const nome = "Luffy"
+let xP = 9000
+
+//
+if((xP > 0) && (xP < 1001)){
+let rank = "ferro"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 1001) && (xP < 2001)){
+let rank = "bronze"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 2001) && (xP < 3001)){
+let rank = "prata"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 3001) && (xP < 4001)){
+let rank = "ouro"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 4001) && (xP < 5001)){
+let rank = "platina"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 5001) && (xP < 6001)){
+let rank = "diamante"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 6001) && (xP < 7001)){
+let rank = "ascendente"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if((xP >= 7001) && (xP < 8001)){
+let rank = "imortal"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+ 
+else if((xP >= 8001) && (xP < 9001)){
+let rank = "diamante"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
+else if(xp >= 9001){
+let rank = "radiante"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+else {
+let rank = "indefinido"
+console.log("O personagem de nome " + nome + " está no Rank " + rank)
+}
+
